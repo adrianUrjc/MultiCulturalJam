d6cc15b011a5e8020d04546e7926a8d60bd3eb32
adrianUrjc <133696658+adrianUrjc@users.noreply.github.com>
2026-02-07 21:45:40 +0100

    v(0.3.2)
    si

--- BandBang/Assets/_Scripts/Player/NPCRecruiter.cs
+++ BandBang/Assets/_Scripts/Player/NPCRecruiter.cs
@@ -5,8 +5,36 @@ using UnityEngine;
 public class NPCRecruiter : MonoBehaviour
 {
     [SerializeField]
-    List<NPCs> npcsRecruited=new();
-   public void RecruitNPC(NPCs npc)
+    public
+    List<NPCs> npcsRecruited = new();
+    public void Start()
+    {
+        LoadNpcs();
+    }
+    public void LoadNpcs()
+    {
+        string npcs = GameManager.Instance.GetComponent<SaveSlot>()
+            .loader.GetValue<string>("NPCsRecruited");
+
+        string[] npcsArray = npcs.Split(',');
+
+        foreach (var npc in npcsArray)
+        {
+            string trimmed = npc.Trim(); // MUY IMPORTANTE
+            npcsRecruited.Add(StringToNpc(trimmed));
+        }
+    }
+
+    public NPCs StringToNpc(string npc)
+    {
+        if (System.Enum.TryParse(npc, out NPCs result))
+            return result;
+
+        Debug.LogError($"NPC desconocido: {npc}");
+        return default; // o lanza excepción si prefieres
+    }
+
+    public void RecruitNPC(NPCs npc)
     {
         if (npcsRecruited.Contains(npc))
         {
@@ -17,12 +45,18 @@ public class NPCRecruiter : MonoBehaviour
     }
     public void RecruitNPC(int npc)
     {
-       
+
         RecruitNPC((NPCs)npc);
+        GameManager.Instance.GetComponent<SaveSlot>().loader.SetValue<string>("NPCsRecruited", NpcsListToString());
     }
     public void PlayNPCMusic()
     {
         NPCMusicPlayer.PlayNPCs(npcsRecruited.ToArray());
+    }
+    public string NpcsListToString()
+    {
+
+        return string.Join(",", npcsRecruited);
     }
     public void StopMusic()
     {
